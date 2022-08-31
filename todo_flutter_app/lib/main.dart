3af6ae99903cc8786f9e98ef30eb9c5595c82d28
stratyd <stratyd@abv.bg>
2022-09-01 00:49:12 +0300

    All functionality of flutter to do application.

---
+++ todo_flutter_app/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:todo_flutter_app/util/dbhelper.dart';
+import 'package:todo_flutter_app/model/todo.dart';
+import 'package:todo_flutter_app/screens/todolist.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    // List<Todo> todos = List<Todo>();
+    // DbHelper helper = DbHelper();
+    // helper.initializeDb().then((result) => helper.getTodos().then((result) => todos = result));
+    // DateTime today = DateTime.now();
+    // Todo todo = Todo("Buy Apples", 1, today.toString(), "And make sure they are good");
+    // helper.insertTodo(todo);
+
+    return new MaterialApp(
+      title: 'Todos',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: MyHomePage(title: 'Todos'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: TodoList(),
+    );
+  }
+}
